88ba639e327f6a69ccc6371eb46c0af4e2dadf10
Cherif436 <66637230+Cherif436@users.noreply.github.com>
2021-06-27 10:26:36 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     env: {
         "BASE_URL": "https://materielinformatique.herokuapp.com",
-        "MONGODB_URL": "mongodb://localhost/nextjsecommerce",
-        "ACCESS_TOKEN_SECRET": "629682136111687",
-        "REFRESH_TOKEN_SECRET": "bsbo8zsdToV6SBOSAI2NaDodUbo",
-        "PAYPAL_CLIENT_ID": "AerrZffa9dmR_WS2nafZk_LGAQ0lH_H-YPHQhYuyv8GOr2bX-ITR5rbkdN9bSk6iTwr7otPp3fAK6bq6",
-        "CLOUD_UPDATE_PRESET": "nextjs_store",
-        "CLOUD_NAME": "siteeco",
-        "CLOUD_API": "https://api.cloudinary.com/v1_1/siteeco/image/upload"
+        "MONGODB_URL": "",
+        "ACCESS_TOKEN_SECRET": "",
+        "REFRESH_TOKEN_SECRET": "",
+        "PAYPAL_CLIENT_ID": "",
+        "CLOUD_UPDATE_PRESET": "",
+        "CLOUD_NAME": "",
+        "CLOUD_API": ""
     }
 }
